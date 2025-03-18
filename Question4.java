4519f339a0eb43d1b952643ccec941f25d91a412
rukiyean <55942069+rukiyean@users.noreply.github.com>
2025-03-18 14:59:34 +0300

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -2,46 +2,58 @@ package Sorular;
 
 import java.util.Scanner;
 
-public class Question2 {
+public class Question4 {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		//8.2 (Sum the major diagonal in a matrix)
-        System.out.println("Enter a 4-by-4 matrix row by row:");
+		8.4 (Compute the weekly hours for each employee) 
+	/*double[][] list = new double[8][7];
         
-        double[][] list = new double[4][4];
-        
-        for (int i = 0; i < 4; i++) {
-        	for (int j = 0; j < 4; j++)
-        	{
-        		list[i][j] = input.nextDouble();
-        	}	
+    for (int i = 0; i < 8; i++) {
+    	for (int j = 0; j < 7; j++)
+    	{
+    		list[i][j] = input.nextDouble();
+    	}	
+    }
+    
+    System.out.println("Calisan ve günler");
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 7; j++) {
+            System.out.printf("%6.2f ", list[i][j]);
         }
-        
-        System.out.println("\nMatrix:");
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.printf("%6.2f ", list[i][j]);
-            }
-            System.out.println();
-        }
-        
-        System.out.println(" ");
-        System.out.println("Sum of the elements in the major diagonal is"+sumMajorDiagonal(list));
+        System.out.println();
+    }	
+    employee(list);
 
 	}
 		
 		
-	public static double sumMajorDiagonal(double[][] m)
-	{
-		int total = 0;
-		 for (int i = 0; i < m.length; i++) {
-			  for (int j = 0; j< m[i].length; j++)
-			  {
-				  total+=m[i][j];
-			  }
-	        }
-		 return total;
+public static void employee( double[][] list)
+{
+int total = 0;
+int[] ytotal = new int[list.length];
+ for (int i = 0; i < list.length; i++) {
+	  for (int j = 0; j< list[i].length; j++)
+	  {
+		  total+=list[i][j];
+	  }
+	  ytotal[i]=total;
+}
+ for (int i = 0; i < list.length-1; i++) {
+    for (int j = 0; j < list.length-i-1; j++) {
+	if (ytotal[i] < ytotal[j]) {
+	    // Swap the numbers
+	    int tempNum = ytotal[i];
+	    ytotal[i] = ytotal[j];
+	    ytotal[j] = tempNum;
+
 	}
+    }
+}
+System.out.println("Sorted list of employee:");
+for (int i = 0; i < list.length; i++) {
+    System.out.println("employe: " + i + ", day: " + ytotal[i]);
+}	 
+}
 
 }
