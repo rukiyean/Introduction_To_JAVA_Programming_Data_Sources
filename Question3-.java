2df524449fcc45f489e0d9e0541a63093e8e4d7b
rukiyean <55942069+rukiyean@users.noreply.github.com>
2025-03-18 14:58:07 +0300

    Update Question3-.java

--- Question3-.java
+++ Question3-.java
@@ -2,44 +2,53 @@ package Sorular;
 
 import java.util.Scanner;
 
-public class Question1 {
+public class Question3 {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-	     System.out.println("Enter a 3-by-4 matrix row by row:");
-	        
-	        double[][] list = new double[3][4];
-	        
-	        for (int i = 0; i < 3; i++) {
-	        	for (int j = 0; j < 4; j++)
-	        	{
-	        		list[i][j] = input.nextDouble();
-	        	}	
-	        }
-	        
-	        System.out.println("\nMatrix:");
-	        for (int i = 0; i < 3; i++) {
-	            for (int j = 0; j < 4; j++) {
-	                System.out.printf("%6.2f ", list[i][j]);
-	            }
-	            System.out.println();
-	        }
-	        
-	        System.out.println(" ");
-	        for (int j = 0; j < 4; j++) {
-	            double sum = sumColumn(list, j);
-	        	System.out.println("Sum of the elements at column " + j + " is "+ sum);
+	     (Sort students on grades) 
+	char[][] answers = {
+			  {'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+			  {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},
+			  {'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'},
+			  {'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'},
+			  {'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+			  {'B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+			  {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+			  {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}};
+	char[] keys = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
 
-	        }
+	int[] correctCounts = new int[answers.length];
+	for (int i = 0; i < answers.length; i++) {
+		   int correctCount = 0;
+		   for (int j = 0; j < answers[i].length; j++) {
+				 if (answers[i][j] == keys[j])
+					 correctCount++;
+			}
+		   correctCounts[i] = correctCount;
+	}
 
-		}
-		public static double sumColumn(double [][] list, int columnIndex)
-		{
-			int total = 0;
-			 for (int i = 0; i < list.length; i++) {
-		        		total += list[i][columnIndex];
-		        }
-			 return total;
-		}
+	sortStudents(answers, correctCounts);
+
+	for (int i = 0; i < correctCounts.length; i++) {
+        System.out.println("Student " + i + "'s correct count is " + correctCounts[i]);
+    }
+}
+	public static void sortStudents( char[][] answers ,int[] correctCounts)
+	{
+	for (int i = 0; i < answers.length-1; i++) {
+            for (int j = 0; j < answers.length-i-1; j++) {
+            	 if (correctCounts[j] < correctCounts[j + 1]) {
+            		 int tempCount = correctCounts[j];
+                     correctCounts[j] = correctCounts[j + 1];
+                     correctCounts[j + 1] = tempCount;    
+                     
+                     char[] tempAnswers = answers[j];
+                     answers[j] = answers[j + 1];
+                     answers[j + 1] = tempAnswers;
+                }
+            }
+        }
+      }
 
 }
