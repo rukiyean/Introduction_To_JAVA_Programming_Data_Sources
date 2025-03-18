6648a65dbd78f3f1516b270152b86b6a61c66c4c
rukiyean <55942069+rukiyean@users.noreply.github.com>
2025-03-18 15:07:28 +0300

    Update Question3-.java

--- Question3-.java
+++ Question3-.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Question3 {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-	     (Sort students on grades) 
+	Scanner input = new Scanner(System.in);
+	//8.3 (Sort students on grades) 
 	char[][] answers = {
 			  {'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
 			  {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},
@@ -36,19 +36,19 @@ public class Question3 {
 }
 	public static void sortStudents( char[][] answers ,int[] correctCounts)
 	{
-	for (int i = 0; i < answers.length-1; i++) {
-            for (int j = 0; j < answers.length-i-1; j++) {
-            	 if (correctCounts[j] < correctCounts[j + 1]) {
-            		 int tempCount = correctCounts[j];
-                     correctCounts[j] = correctCounts[j + 1];
-                     correctCounts[j + 1] = tempCount;    
-                     
-                     char[] tempAnswers = answers[j];
-                     answers[j] = answers[j + 1];
-                     answers[j + 1] = tempAnswers;
-                }
-            }
-        }
+		for (int i = 0; i < answers.length-1; i++) {
+	            for (int j = 0; j < answers.length-i-1; j++) {
+	            	 if (correctCounts[j] < correctCounts[j + 1]) {
+	            		 int tempCount = correctCounts[j];
+	                     correctCounts[j] = correctCounts[j + 1];
+	                     correctCounts[j + 1] = tempCount;    
+	                     
+	                     char[] tempAnswers = answers[j];
+	                     answers[j] = answers[j + 1];
+	                     answers[j + 1] = tempAnswers;
+	                }
+	            }
+	        }
       }
 
 }
